03c21dc85842e9f15438d68d31c206047142515c
zuhriD <dikizuhri@gmail.com>
2026-08-06 12:56:32 +0700

    fix: correct project name in package-lock.json and update UserFactory to use faker instance

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,9 +25,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'username' => fake()->unique()->userName(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'username' => $this->faker->unique()->userName(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
